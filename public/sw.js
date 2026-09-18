93c74157a28774170f29eaffcc2d22fa3d81112b
fernandojaborges-sys <fernandojaborges@gmail.com>
2026-09-18 08:07:57 -0300

    chore(sw): bump cache pra v2.9.3 (forca reload do ChatSecretaria no PWA)

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,7 @@
  * mesmo sem internet (depois de carregado pelo menos 1x online).
  */
 
-const CACHE_NAME = "kairos-igreja-v2.8.5";
+const CACHE_NAME = "kairos-igreja-v2.9.3";
 const STATIC_ASSETS = [
   "/",
   "/privacidade",
